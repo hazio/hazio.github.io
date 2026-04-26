ec32e00ec79aa088fe24902e1c96d2b2deb86a8c
Jani Halmetoja <jani.halmetoja@valmet.com>
2026-04-26 10:54:51 +0300

    SEO enhancements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://halmetojamodel.com",
+  integrations: [sitemap()],
 });
